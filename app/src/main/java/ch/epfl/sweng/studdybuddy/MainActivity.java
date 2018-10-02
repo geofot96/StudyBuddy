51f6f6fb413250fcc74aeadbdc21d22743ecf0d5
Gerald Sula <gerald.sula@epfl.ch>
2018-10-02 18:20:02 +0200

    Add simple open camera button

--- app/src/main/java/ch/epfl/sweng/studdybuddy/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/MainActivity.java
@@ -1,7 +1,13 @@
 package ch.epfl.sweng.studdybuddy;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +17,32 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button openCamera=(Button)findViewById(R.id.cameraButton);
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                }
+            }
+        });
     }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            setContentView(R.layout.display_photo);
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageTaken=(ImageView)findViewById(R.id.imageView);
+            imageTaken.setImageBitmap(imageBitmap);
+
+        }
+    }
+
 }
