80258d6a812f909bf07b891c6e1154b3faadd181
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2018-10-03 18:15:29 +0200

    Add multiple activities to design the basic structure

---
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/GroupsActivityvol2.java
@@ -0,0 +1,24 @@
+package ch.epfl.sweng.studdybuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GroupsActivityvol2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups_activityvol2);
+        Intent other = getIntent();
+    }
+
+    public void gotoCreation(View view)
+    {
+        Intent intent = new Intent(this, CreateGroup.class);
+        startActivity(intent);
+    }
+}
