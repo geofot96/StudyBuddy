e98f0e75356225699f18df464a2de982bf5823b3
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2018-10-08 23:00:22 +0200

    Created user class tests

--- app/src/main/java/ch/epfl/sweng/studdybuddy/User.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/User.java
@@ -2,41 +2,51 @@ package ch.epfl.sweng.studdybuddy;
 
 import java.util.ArrayList;
 
-public class User {
+public class User
+{
     private String id;
     private String name;
-    private @DummyCourses.Section String Section;
+    private @DummyCourses.Section
+    String Section;
     //private Schedule schedule;
     private ArrayList<Group> currentGroups;
 
-    public User(String id, String name, String section, ArrayList<Group> currentGroups) {
+    public User(String id, String name, String section, ArrayList<Group> currentGroups)
+    {
         this.id = id;
         this.name = name;
         Section = section;
         this.currentGroups = currentGroups;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public String getSection() {
+    public String getSection()
+    {
         return Section;
     }
 
-    public void setSection(String section) {
+    public void setSection(String section)
+    {
         Section = section;
     }
 
-    public ArrayList<Group> getCurrentGroups() {
+    public ArrayList<Group> getCurrentGroups()
+    {
         return currentGroups;
     }
 
-    public void setCurrentGroups(ArrayList<Group> currentGroups) {
+    //TODO why do we want this method?? Maybe have an add to group method or something like that?
+    public void setCurrentGroups(ArrayList<Group> currentGroups)
+    {
         this.currentGroups = currentGroups;
     }
 }
