775ce42a0e7634a67778f0823fe6aa88892a5aaa
Frederic Gessler <frederic.gessler@epfl.ch>
2018-10-08 12:03:03 +0200

    Fixed card deletion

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/CourseSelectActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/CourseSelectActivityTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.studdybuddy;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.AutoCompleteTextView;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,8 +15,12 @@ import static android.support.test.espresso.action.ViewActions.pressImeActionBut
 import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 //Suggestions refers to courses database
 
@@ -27,40 +32,36 @@ public class CourseSelectActivityTest {
 
     @Test
     public void skipLeadsToMainActivity() {
-        onView(withId(R.id.skipButton)).perform(click()).check();
+        //onView(withId(R.id.skipButton)).perform(click()).check();
+    }
+
+    @Test
+    public void doneIsHiddenIfNoCourseSelected() {
+
     }
 
     @Test//()
     public void clickOnCourseFieldOpensSuggestions() {
-        onView(withId(R.id.courseField)).perform(click());
-        onView(withId(R.id.courseComplete));
+        onView(withId(R.id.courseComplete)).perform(click());
+        //onView(withId(R.id.courseComplete)).check(matches(isDisplayed()));
+        AutoCompleteTextView tv;
     }
 
     @Test //()
-    public void suggestionsDntGrowWhenLetterTyped() {
-
-    }
-
-    @Test //()
-    public void suggestionsEmptyWhenGibberishEntered() {
-
-    }
-
-    @Test
-    public void enterIsDisabledWhenInvalidInput() {
+    public void courseNotAddedIfGibberish() {
+        onView(withId(R.id.courseComplete)).perform(click(), typeText("yxcvbn"), pressImeActionButton());
+        //onView(withId(R.id.coursesSet)).check()
 
     }
 
     @Test //()
     public void enterOnValidInputAddsCourseToList() {
-        onView(withId(R.id.courseField)).perform(typeText(coursesDB[0]));
-        onView(withId(R.id.courseField)).perform(pressImeActionButton());
-        onView(withId(R.id.courseView)).check(matches());
+        onView(withId(R.id.courseComplete)).perform(pressImeActionButton());
     }
 
     @Test
     public void clickOnCourseSuggestionAddsCourseToList() {
-        onView(withId(R.id.courseField)).perform();
+        onView(withId(R.id.courseComplete)).perform(click());
     }
 
     //swipe on course
--- app/src/main/java/ch/epfl/sweng/studdybuddy/CourseSelectActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/CourseSelectActivity.java
@@ -1,10 +1,12 @@
 package ch.epfl.sweng.studdybuddy;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -34,7 +36,7 @@ public class CourseSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_select);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, coursesDB);
         textView = (AutoCompleteTextView)findViewById(R.id.courseComplete);
         textView.setAdapter(adapter);
@@ -50,17 +52,33 @@ public class CourseSelectActivity extends AppCompatActivity {
         textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String textInput = parent.getItemAtPosition(position).toString();
+                String textInput = parent.getAdapter().getItem(position).toString();
                 if(!courseSelection.contains(textInput))
                     addCourse(textInput);
             }
         });
         //courseSelection.add("Wine tasting");
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.coursesSet);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.coursesSet);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         recyclerView.setAdapter(new CourseAdapter(courseSelection));
+        ItemTouchHelper mIth = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                        ItemTouchHelper.RIGHT) {
+                    @Override
+                    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                        CourseHolder cc = (CourseHolder)viewHolder;
+                        courseSelection.remove(courseSelection.indexOf(cc.get()));
+                        recyclerView.getAdapter().notifyDataSetChanged();
+                    }
+                });
+        mIth.attachToRecyclerView(recyclerView);
         textView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
@@ -72,17 +90,14 @@ public class CourseSelectActivity extends AppCompatActivity {
                     if (event.getAction()!=KeyEvent.ACTION_DOWN) return true;
                 }
                 else  return false;
-                //Unnecessary
-                textView.performCompletion();
-                textView.performValidation();
                 //Protection
                 String textInput = textView.getText().toString();
                 if(Arrays.asList(coursesDB).contains(textInput) && !courseSelection.contains(textInput)) addCourse(textInput);
                 return true;
             }
         });
-        Button skipButton = (Button)findViewById(R.id.skipButton);
-        Button doneButton = (Button)findViewById(R.id.doneButton);
+        Button skipButton =findViewById(R.id.skipButton);
+        Button doneButton =findViewById(R.id.doneButton);
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -108,6 +123,8 @@ public class CourseSelectActivity extends AppCompatActivity {
         textView.setText("");
     }
 
-
+    private void removeCourse(String course) {
+        courseSelection.remove(course);
+    }
 
 }
