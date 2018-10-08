df176cb7ff725e890b7cbe1aef58a78f23f87ded
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2018-10-08 11:24:03 +0200

    Merged issue42 with current branch

--- app/src/main/java/ch/epfl/sweng/studdybuddy/CourseSelectActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/CourseSelectActivity.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.studdybuddy;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -34,6 +35,9 @@ public class CourseSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_select);
+
+        Intent other = getIntent();
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, coursesDB);
         textView = (AutoCompleteTextView)findViewById(R.id.courseComplete);
