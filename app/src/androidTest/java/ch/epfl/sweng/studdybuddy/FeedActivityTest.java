89288cbbbf40eed0043e1eb485d87f829f7d0369
Frederic Gessler <frederic.gessler@epfl.ch>
2018-10-12 19:32:14 +0200

    Wrote half of the tests specifications/contract about FirebaseWrapper and FeedActivity

---
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/FeedActivityTest.java
@@ -0,0 +1,55 @@
+package ch.epfl.sweng.studdybuddy;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FeedActivityTest {
+    @Test
+    void pullingTheFeedDownRefreshes() {
+
+    }
+
+    @Test
+    void feedCardXisAboveYifYounger() {
+
+    }
+
+    @Test
+    void failedConnectionShowsErrorMessage() {
+
+    }
+
+    @Test
+    void clickOnCardExpandsIt() {
+
+    }
+
+    @Test
+    void feedCardIsVisibleIfNotFull() {
+
+    }
+
+    @Test
+    void onStartupCourseDBIsLoaded() {
+
+    }
+
+    @Test
+    void onResumeCourseDBIsAvailable() {
+
+    }
+
+    @Test
+    void onGroupCreateGroupUpdateDB() {
+
+    }
+
+    @Test
+    void onGroupJoinUpdate() {
+
+    }
+
+}
