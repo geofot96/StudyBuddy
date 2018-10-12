7187d29cc3fd6e0b77c836185b24b67b09e07fa3
underwoodkats <underwoodkats@gmail.com>
2018-10-12 16:50:14 +0200

    added profile course list activity

--- app/src/main/java/ch/epfl/sweng/studdybuddy/ProfileTab.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/ProfileTab.java
@@ -1,13 +1,21 @@
 package ch.epfl.sweng.studdybuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ProfileTab extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "ch.epfl.sweng.studdybuddy.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_tab);
+    }
+
+    public void openCoursesActivity(View view) {
+        Intent intent = new Intent(this, ProfileCourses.class);
+        startActivity(intent);
     }
 }
