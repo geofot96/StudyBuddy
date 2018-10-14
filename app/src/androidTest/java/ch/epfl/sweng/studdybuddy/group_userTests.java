06d4524d84b49bb54423398e986ee89bcc808e0f
Frederic Gessler <frederic.gessler@epfl.ch>
2018-10-15 01:13:03 +0200

    Deleted unimplemented FeedActivity, fixed group_userTests

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/group_userTests.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/group_userTests.java
@@ -14,7 +14,6 @@ public class group_userTests
     @Test
     public void canAddUser()
     {
-        DummyCourses listOfDummyCourses = new DummyCourses();
         Group g1 = new Group(5, new Course("test"), "fr", new ArrayList<User>());
         User u1 = new User("xxx@yyy.zzz", "User One", MATHEMATICS, new ArrayList<Group>(), null);
         g1.addParticipant(u1);
