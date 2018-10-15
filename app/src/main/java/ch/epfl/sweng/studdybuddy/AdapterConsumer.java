f079549bf69220ccf185cba49e337667e3c51945
Frederic Gessler <frederic.gessler@epfl.ch>
2018-10-16 01:24:14 +0200

    Consumer copycat (no JAVA8:()

---
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/AdapterConsumer.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.studdybuddy;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+final public class AdapterConsumer {
+
+    private AdapterConsumer() {}
+    public static <T> Consumer<List<T>> adapterConsumer(Class<T> type, final List<T> set, final RecyclerView.Adapter adapter) {
+        return new Consumer<List<T>>() {
+            @Override
+            public void accept(List<T> list) {
+                set.removeAll(set);
+                set.addAll(list);
+                adapter.notifyDataSetChanged();
+            }
+        };
+    }
+    public static <T> Consumer<List<T>> adapterConsumer(Class<T> type, final List<T> set, final ArrayAdapter<T> adapter) {
+        return new Consumer<List<T>>() {
+            @Override
+            public void accept(List<T> list) {
+                set.removeAll(set);
+                set.addAll(list);
+                adapter.notifyDataSetChanged();
+            }
+        };
+    }
+}
