f7f7df13df9860d2b99214b17e75cfac6ab9ee6a
Yann BOUQUET <y.bouquet29@gmail.com>
2018-10-22 15:00:17 +0200

    add AccountTest to test Account class

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/activities/ProfileTabTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/activities/ProfileTabTest.java
@@ -1,4 +1,4 @@
-package ch.epfl.sweng.studdybuddy;
+package ch.epfl.sweng.studdybuddy.activities;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
