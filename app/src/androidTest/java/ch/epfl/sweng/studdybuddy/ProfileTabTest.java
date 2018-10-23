2fd1e3324bc974f0811d77b355358c07e14e08c1
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2018-10-23 20:21:03 +0200

    Updated ProfileTab and profileTab Tests to be up to date with master

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/ProfileTabTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/ProfileTabTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
-import ch.epfl.sweng.studdybuddy.activities.ProfileTab;
+import ch.epfl.sweng.studdybuddy.ProfileTab;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
