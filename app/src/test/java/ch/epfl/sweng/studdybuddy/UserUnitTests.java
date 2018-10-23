3660f674a8fb1cf39840c19dd5ac2263f62416e9
Gerald Sula <gerald.sula@epfl.ch>
2018-10-23 20:22:33 +0200

    finish User test

--- app/src/test/java/ch/epfl/sweng/studdybuddy/UserUnitTests.java
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/UserUnitTests.java
@@ -3,25 +3,61 @@ package ch.epfl.sweng.studdybuddy;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class UserUnitTests
 {
-    private static User user = new User( "Mr Potato", new ID<>("dumbid"), new ArrayList<>());
+    private static ArrayList<String> list= new ArrayList<String>(Arrays.asList("aa","bb"));
+
+    private static User user = new User( "Mr Potato", new ID<>("dumbid"),  list);
+    private  static ID id=new ID("someId");
 
     @Test
-    public void getNameWorksCorrectly()
+    public void testGetNameWorksCorrectly()
     {
-        User user1 = new User( "Mr Potato", new ID<>("dumbid"), new ArrayList<>());
+        User user1 = new User( "Mr Potato", new ID<>("dumbid"), list);
         assertEquals("Mr Potato", user1.getName());
     }
 
     @Test
-    public void setNameWorksCorrectly()
+    public void TestSetNameWorksCorrectly()
     {
         user.setName("Geo");
         assertEquals("Geo", user.getName());
     }
+
+    @Test
+    public void testEmptyConstructor()
+    {
+        User x=new User();
+
+    }
+    @Test
+    public void testGetCoursesPreset()
+    {
+        User user1 = new User( "Mr Potato", new ID<>("dumbid"), list);
+        assertEquals(list, user1.getCoursesPreset());
+    }
+    @Test
+    public void testSetCoursesPreset()
+    {
+        user.setCoursesPreset(list);
+        assertEquals(list, user.getCoursesPreset());
+    }
+    @Test
+    public void testSetUserID()
+    {
+        user.setUserID(id);
+        assertEquals(id, user.getUserID());
+    }
+    @Test
+    public void testGetUserID()
+    {
+        User user1 = new User( "Mr Potato", id, list);
+        assertEquals(id, user1.getUserID());
+    }
+
 
 }
