21f636ac58c2de31d3dac2512548d0d8bd88e2b0
Yann BOUQUET <y.bouquet29@gmail.com>
2018-10-24 04:53:34 +0200

    add tests for CourseSelectActivity

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/CourseSelectActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/CourseSelectActivityTest.java
@@ -1,11 +1,13 @@
 package ch.epfl.sweng.studdybuddy;
 
+import android.content.ComponentName;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.GeneralLocation;
 import android.support.test.espresso.action.GeneralSwipeAction;
 import android.support.test.espresso.action.Press;
 import android.support.test.espresso.action.Swipe;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -22,6 +24,7 @@ import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.studdybuddy.CourseSelectActivity;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 
@@ -58,23 +61,23 @@ import static org.hamcrest.Matchers.not;
 @RunWith(AndroidJUnit4.class)
 public class       CourseSelectActivityTest
 {
-    public static final String mockCourse = "Algorithms";
+    public static final String mockCourse = "Concurrent algorithms CS-453";
     @Rule
-    public final ActivityTestRule<CourseSelectActivity> mActivityRule =
-            new ActivityTestRule<>(CourseSelectActivity.class);
+    public final IntentsTestRule<CourseSelectActivity> mActivityRule =
+            new IntentsTestRule<>(CourseSelectActivity.class);
 
     @Test
-    public void skipLeadsToMainActivity()
+    public void skipLeadsToGroupActivity()
     {
-        //onView(withId(R.id.skipButton)).perform(click());
-        //intended(toPackage(MainActivity.class.getName()));
+        onView(withId(R.id.skipButton)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), GroupsActivity.class)));
     }
 
 
     @Test
     public void doneIsHiddenIfNoCourseSelected()
     {
-        //onView(withId(R.id.doneButton)).check(matches(not(isEnabled())));
+        onView(withId(R.id.doneButton)).check(matches(not(isEnabled())));
         //select a course
         //delete a course
         //type a course
@@ -83,8 +86,9 @@ public class       CourseSelectActivityTest
 
     @Test //()
     public void courseNotAddedIfGibberish() {
-        //onView(withId(R.id.courseComplete)).perform(click(), typeText("yxcvbn"), pressImeActionButton());
-        //onView(withId(R.id.coursesSet)).check(matches(not(hasDescendant(withText("yxcvbn")))));
+        onView(withId(R.id.courseComplete)).perform(click(), typeText("yxcvbn"));
+        onView(withId(R.id.doneButton)).perform(closeSoftKeyboard());
+        onView(withId(R.id.doneButton)).check(matches(not(isEnabled())));
     }
 
     @Test //()
@@ -96,35 +100,40 @@ public class       CourseSelectActivityTest
 
     @Test
     public void clickOnCourseSuggestionAddsCourseToList() {
-        // onView(withId(R.id.courseComplete)).perform(click());
-        // onData(equalTo(mockCourse)).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        // onView(withId(R.id.coursesSet)).check(matches(hasDescendant(withText(mockCourse))));
+        onView(withId(R.id.courseComplete)).perform(click(), typeText("concurrent"));
+        onData(equalTo(mockCourse)).inRoot(RootMatchers.isPlatformPopup()).perform(click());
+        onView(withId(R.id.courseComplete)).perform(closeSoftKeyboard());
+        onView(withId(R.id.coursesSet)).check(matches(hasDescendant(withText(mockCourse))));
     }
 
     //swipe on course
     @Test
     public void swipeOnCourseDeletesIt() {
+        onView(withId(R.id.courseComplete)).perform(click(), typeText("concurrent"));
+        onData(equalTo(mockCourse)).inRoot(RootMatchers.isPlatformPopup()).perform(click());
+        onView(withId(R.id.courseComplete)).perform(closeSoftKeyboard());
+        onView(withId(R.id.coursesSet)).check(matches(hasDescendant(withText(mockCourse))));
         // onView(withId(R.id.courseComplete)).perform(click(), typeText(mockCourse), pressKey(KeyEvent.KEYCODE_ENTER));
-        // onView(allOf(is(instanceOf(TextView.class)), withText(mockCourse), isDescendantOfA(withId(R.id.coursesSet)))).perform(withCustomConstraints(swipeLeft(), isDisplayingAtLeast(1)));
-        // onView(withId(R.id.courseComplete)).check(matches(not(hasDescendant(withText(mockCourse)))));
+        onView(allOf(is(instanceOf(TextView.class)), withText(mockCourse), isDescendantOfA(withId(R.id.coursesSet)))).perform(withCustomConstraints(swipeRight(), isDisplayingAtLeast(1)));
+        onView(withId(R.id.courseComplete)).check(matches(not(hasDescendant(withText(mockCourse)))));
     }
 
-//    public static ViewAction withCustomConstraints(final ViewAction action, final Matcher<View> constraints) {
-//        return new ViewAction() {
-//            @Override
-//            public Matcher<View> getConstraints() {
-//                return constraints;
-//            }
-//
-//            @Override
-//            public String getDescription() {
-//                return action.getDescription();
-//            }
-//
-//            @Override
-//            public void perform(UiController uiController, View view) {
-//                action.perform(uiController, view);
-//            }
-//        };
-//    }
+    public static ViewAction withCustomConstraints(final ViewAction action, final Matcher<View> constraints) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return constraints;
+            }
+
+            @Override
+            public String getDescription() {
+                return action.getDescription();
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                action.perform(uiController, view);
+            }
+        };
+    }
 }
