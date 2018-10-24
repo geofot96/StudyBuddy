9ecea1b2dbe8aa0b7f365d60c1f48068cd3f1f08
Yann BOUQUET <y.bouquet29@gmail.com>
2018-10-24 03:50:11 +0200

    add tests in CreateGroupTest

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/CreateGroupTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/CreateGroupTest.java
@@ -1,9 +1,13 @@
 package ch.epfl.sweng.studdybuddy;
 
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -23,18 +27,23 @@ import org.junit.runner.RunWith;
 import ch.epfl.sweng.studdybuddy.R;
 import ch.epfl.sweng.studdybuddy.activities.CreateGroupActivity;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
@@ -43,11 +52,18 @@ import static org.hamcrest.Matchers.is;
 public class CreateGroupTest {
 
     @Rule
-    public ActivityTestRule<CreateGroupActivity> mActivityTestRule = new ActivityTestRule<>(CreateGroupActivity.class);
+    public IntentsTestRule<GroupsActivity> mIntentsTestRule = new IntentsTestRule<>(GroupsActivity.class);
 
     @Test
-    public void createAGroupWillBeRegistered() {
-  /*      // Added a sleep statement to match the app's execution delay.
+    public void createAGroupWillBeRegistered() throws InterruptedException {
+
+        onView(withId(R.id.createGroup)).perform(click());
+        onView(withId(R.id.courseComplete2)).perform(click(), typeText("algo"));
+        onData(equalTo("Concurrent algorithms CS-453")).inRoot(RootMatchers.isPlatformPopup()).perform(click()).perform(closeSoftKeyboard());
+        onView(withId(R.id.confirmGroupCreation)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), GroupsActivity.class)));
+
+       /*// Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
@@ -162,8 +178,15 @@ public class CreateGroupTest {
                                 2),
                         isDisplayed()));
         textView2.check(matches(withText("Fr")));
+*/
+    }
 
- */   }
+    @Test
+    public void createAGroupeWithoutSettings(){
+        onView(withId(R.id.createGroup)).perform(click());
+        onView(withId(R.id.confirmGroupCreation)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), CreateGroupActivity.class)));
+    }
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
