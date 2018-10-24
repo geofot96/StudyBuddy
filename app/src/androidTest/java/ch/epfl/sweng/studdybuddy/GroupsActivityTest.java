95667d83ed691b7b3cc24213670da133f3455835
Yann BOUQUET <y.bouquet29@gmail.com>
2018-10-24 08:22:38 +0200

    add tests in GroupsActivityTest

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/GroupsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/GroupsActivityTest.java
@@ -1,67 +1,39 @@
 package ch.epfl.sweng.studdybuddy;
 
-import android.support.test.espresso.UiController;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.action.GeneralLocation;
-import android.support.test.espresso.action.GeneralSwipeAction;
-import android.support.test.espresso.action.Press;
-import android.support.test.espresso.action.Swipe;
-import android.support.test.espresso.matcher.RootMatchers;
-import android.support.test.rule.ActivityTestRule;
+import android.content.ComponentName;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
 
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ch.epfl.sweng.studdybuddy.GroupsActivity;
+import ch.epfl.sweng.studdybuddy.activities.CreateGroupActivity;
 
-import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.pressKey;
-import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
-import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class GroupsActivityTest
 {
     @Rule
-    public final ActivityTestRule<GroupsActivity> mActivityRule =
-            new ActivityTestRule<>(GroupsActivity.class);
+    public final IntentsTestRule<GroupsActivity> mActivityRule =
+            new IntentsTestRule<>(GroupsActivity.class);
 
     @Test
     public void plusButtonLeadsToCreateGroupActivity()
     {
-        //onView(withId(R.id.createGroup)).perform(click());
-        //intended(hasComponent(CreateGroup.class.getName()));
+        onView(withId(R.id.createGroup)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), CreateGroupActivity.class)));
+    }
+
+    @Test
+    public void WeCanClickOnSortButton() throws InterruptedException {
+        Thread.sleep(1000);
+        onView(withId(R.id.sortButton)).perform(click());
     }
 }
