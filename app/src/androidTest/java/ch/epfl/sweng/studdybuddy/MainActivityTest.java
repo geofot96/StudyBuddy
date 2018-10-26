9ae916b9f3c4e7c198705bb9d9bb28a88e2cc47d
Boubacar Camara <bakarsadio@gmail.com>
2018-10-26 18:20:19 +0200

    Add Thread sleep in MainActivityTest

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MainActivityTest.java
@@ -26,19 +26,22 @@ public class MainActivityTest {
             new IntentsTestRule<>(DummyMainActivity.class);
 
     @Test
-    public void clickGoToGroupsGoToGroupsActivity(){
+    public void clickGoToGroupsGoToGroupsActivity() throws InterruptedException{
+        Thread.sleep(1000);
         onView(withId(R.id.gotoGroups)).perform(click());
         intended(hasComponent(new ComponentName(getTargetContext(), GroupsActivity.class)));
     }
 
     @Test
-    public void clickCourseButtonGoToCourseSelectActivity(){
+    public void clickCourseButtonGoToCourseSelectActivity() throws InterruptedException{
+        Thread.sleep(1000);
         onView(withId(R.id.courseButton)).perform(click());
         intended(hasComponent(new ComponentName(getTargetContext(), CourseSelectActivity.class)));
     }
 
     @Test
-    public void clickProfileButtonGoToCProfileTab(){
+    public void clickProfileButtonGoToCProfileTab() throws InterruptedException{
+        Thread.sleep(1000);
         onView(withId(R.id.go_to_profile_btn)).perform(click());
         intended(hasComponent(new ComponentName(getTargetContext(), ProfileTab.class)));
     }
