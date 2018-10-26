3f907b9ffdfeeb31dc0a383689e44bf764e80185
underwoodkats <underwoodkats@gmail.com>
2018-10-26 20:07:51 +0200

    initialized class calendar

---
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/Calendar.java
@@ -0,0 +1,16 @@
+package ch.epfl.sweng.studdybuddy;
+
+import java.util.List;
+
+public class Calendar {
+
+    private ID<Group> groupID;
+
+    private List<AvailiabilityList> getAvailabilityListsFromFB (){
+
+    }
+    
+    private List<Integer> getGroupAvailability(){
+
+    }
+}
