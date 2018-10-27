ea946567e26a9cf2ee0ca6b0ba71f03ce5964c88
underwoodkats <underwoodkats@gmail.com>
2018-10-27 12:07:49 +0200

    added function to get sum of two arrays

--- app/src/main/java/ch/epfl/sweng/studdybuddy/Calendar.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/Calendar.java
@@ -1,16 +1,31 @@
 package ch.epfl.sweng.studdybuddy;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Calendar {
+public final class Calendar {
 
     private ID<Group> groupID;
 
-    private List<AvailiabilityList> getAvailabilityListsFromFB (){
+    //private List<Availability> currnetAvailability;
+
+    private List<Integer> getSumOfTwoLists(List<Integer> first_list, List<Integer> second_list){
+
+        int len = first_list.size();
+        List<Integer> result = new ArrayList<>(len);
+
+        for (int i = 0; i < len; i++){
+            int element = first_list.get(i) + second_list.get(i);
+            result.add(element);
+        }
+        return result;
+    }
+
+    /*private List<Availiability> getAvailabilityListsFromFB (){
 
     }
     
     private List<Integer> getGroupAvailability(){
 
-    }
+    }*/
 }
