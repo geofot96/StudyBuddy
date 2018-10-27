a2513f6910d307db5f5718e2b51b7042169b36e5
Boubacar Camara <bakarsadio@gmail.com>
2018-10-27 17:02:00 +0200

    Add SerialDate tests

---
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/SerialDateTest.java
@@ -0,0 +1,30 @@
+package ch.epfl.sweng.studdybuddy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SerialDateTest {
+    public SerialDate date;
+    @Before
+    public void setup() {
+        date = new SerialDate();
+    }
+    @Test
+    public void beforeAfterWorks(){
+        SerialDate after = new SerialDate();
+        assert(date.before(after));
+        assert(after.after(date));
+    }
+
+    @Test
+    public void setWorks(){
+        SerialDate d = new SerialDate();
+        d.setDay(10);
+        d.setSeconds(10);
+        d.setMinutes(10);
+        d.setMonth(10);
+        d.setYear(2018);
+        assert(d.getDay() == 10 && d.getYear() == 2018 && d.getSeconds()==10 && d.getMinutes()==10 &&d.getMonth()==10);
+    }
+
+}
