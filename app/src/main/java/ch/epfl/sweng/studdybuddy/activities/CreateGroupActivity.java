f43bdcfb6525a85566b9bdecda62b95a0d5ca907
Frederic Gessler <frederic.gessler@epfl.ch>
2018-11-09 18:02:23 +0100

    Displaying admin

--- app/src/main/java/ch/epfl/sweng/studdybuddy/activities/CreateGroupActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/activities/CreateGroupActivity.java
@@ -14,6 +14,7 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import ch.epfl.sweng.studdybuddy.R;
 import ch.epfl.sweng.studdybuddy.core.Course;
@@ -122,7 +123,7 @@ public class CreateGroupActivity extends AppCompatActivity implements AdapterVie
     {
 
             User user = ((StudyBuddy) CreateGroupActivity.this.getApplication()).authendifiedUser;
-            Group g = new Group(maxParticipants, new Course(selectedCourse),selectedLanguage, user.getUserID().getId());
+            Group g = new Group(maxParticipants, new Course(selectedCourse),selectedLanguage, UUID.randomUUID().toString(), user.getUserID().getId());
             mb.pushGroup(g, user.getUserID().getId());
             createUserInitialAvailabilities(user.getUserID().getId(), g.getGroupID().getId());
 	        Intent intent = new Intent(this, GroupsActivity.class);
