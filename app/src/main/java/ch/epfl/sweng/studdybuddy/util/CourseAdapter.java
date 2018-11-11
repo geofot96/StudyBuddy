e5e8058bd7b40f7a77818a43cda86eca33e6f464
Boubacar Camara <bakarsadio@gmail.com>
2018-11-11 17:34:38 +0100

    Modularize adapters

--- app/src/main/java/ch/epfl/sweng/studdybuddy/util/CourseAdapter.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/util/CourseAdapter.java
@@ -1,31 +1,17 @@
 package ch.epfl.sweng.studdybuddy.util;
 
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import java.util.List;
 
 import ch.epfl.sweng.studdybuddy.R;
 
-public class CourseAdapter extends RecyclerView.Adapter<Holder>
-{
-    List<String> list;
+public class CourseAdapter extends Adapter
+{    List<String> list;
 
     //ajouter un constructeur prenant en entrée une liste
     public CourseAdapter(List<String> list)
     {
+        super( R.layout.course_card, R.id.text);
         this.list = list;
-    }
-
-    //cette fonction permet de créer les viewHolder
-    //et par la même indiquer la vue à inflater (à partir des layout xml)
-    @Override
-    public Holder onCreateViewHolder(ViewGroup viewGroup, int itemType)
-    {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.course_card, viewGroup, false);
-        return new Holder(view, R.id.text);
     }
 
     //c'est ici que nous allons remplir notre cellule avec le texte/image de chaque MyObjects
--- app/src/main/java/ch/epfl/sweng/studdybuddy/util/ParticipantAdapter.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/util/ParticipantAdapter.java
@@ -1,33 +1,21 @@
 package ch.epfl.sweng.studdybuddy.util;
 
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import java.util.List;
 
 import ch.epfl.sweng.studdybuddy.R;
 import ch.epfl.sweng.studdybuddy.core.User;
 
-public class ParticipantAdapter extends RecyclerView.Adapter<Holder> {
+public class ParticipantAdapter extends Adapter {
 
     List<User> participants;
 
     //ajouter un constructeur prenant en entrée une liste
     public ParticipantAdapter(List<User> participants)
     {
+        super(R.layout.participant_card, R.id.participant);
         this.participants = participants;
     }
 
-    //cette fonction permet de créer les viewHolder
-    //et par la même indiquer la vue à inflater (à partir des layout xml)
-    @Override
-    public Holder onCreateViewHolder(ViewGroup viewGroup, int itemType)
-    {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.participant_card, viewGroup, false);
-        return new Holder(view, R.id.participant);
-    }
 
     //c'est ici que nous allons remplir notre cellule avec le texte/image de chaque MyObjects
     @Override
