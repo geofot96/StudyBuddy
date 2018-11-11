a0a28860b26e0f3ca7595bf60a399b6037471503
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2018-11-11 19:46:24 +0100

    Added onClick functionality to calendar button in the nav bar

--- app/src/main/java/ch/epfl/sweng/studdybuddy/activities/NavigationActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/activities/NavigationActivity.java
@@ -1,17 +1,59 @@
 package ch.epfl.sweng.studdybuddy.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import ch.epfl.sweng.studdybuddy.Fragments.MergedCalendarFragment;
 import ch.epfl.sweng.studdybuddy.R;
 
 public class NavigationActivity extends AppCompatActivity
 {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private MergedCalendarFragment mergedCalendarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        mergedCalendarFragment = new MergedCalendarFragment();
+
+        //setFragment(mergedCalendarFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch(menuItem.getItemId())
+                {
+                    case R.id.navToCalendar:
+                        setFragment(mergedCalendarFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
