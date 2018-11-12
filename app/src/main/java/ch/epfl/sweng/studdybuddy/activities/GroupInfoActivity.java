bc6f8cffa4b0e4da64fe42adca35c64043a332fe
Boubacar Camara <bakarsadio@gmail.com>
2018-11-13 00:54:45 +0100

    Remove useless condition

--- app/src/main/java/ch/epfl/sweng/studdybuddy/activities/GroupInfoActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/activities/GroupInfoActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -55,16 +54,5 @@ public class GroupInfoActivity extends AppCompatActivity{
                 }
             }
         });
-        /*for(User u: participants) {
-            Log.i("SPCIAL", u.getUserID().getId() + "::" + uId);
-            if(u.getUserID().getId().equals(uId)) {
-                button.setVisibility(View.VISIBLE);
-            }
-        }*/
     }
-
-
-
-
-
 }
