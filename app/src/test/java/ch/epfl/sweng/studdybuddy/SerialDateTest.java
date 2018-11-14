142d0dc1cedfffaa2e1394b0a4e31c35e97c18eb
underwoodkats <underwoodkats@gmail.com>
2018-11-14 16:40:26 +0100

    uncommented one tests that works

--- app/src/test/java/ch/epfl/sweng/studdybuddy/SerialDateTest.java
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/SerialDateTest.java
@@ -11,12 +11,13 @@ public class SerialDateTest {
     public void setup() {
         date = new SerialDate();
     }
-    /*@Test
+
+    @Test
     public void beforeAfterWorks(){
         SerialDate after = new SerialDate();
         assert(date.before(after));
         assert(after.after(date));
-    }*/
+    }
 
     @Test
     public void setWorks(){
