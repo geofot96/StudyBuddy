d096af9c9dc7f1046c0711bbb5849297c5fcab5f
Frederic Gessler <frederic.gessler@epfl.ch>
2018-11-15 22:14:38 +0100

    Push meeting implemented and tested

---
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/firebase/MetaMeeting.java
@@ -0,0 +1,24 @@
+package ch.epfl.sweng.studdybuddy.firebase;
+
+import ch.epfl.sweng.studdybuddy.core.Group;
+import ch.epfl.sweng.studdybuddy.core.ID;
+import ch.epfl.sweng.studdybuddy.core.Meeting;
+import ch.epfl.sweng.studdybuddy.core.Pair;
+import ch.epfl.sweng.studdybuddy.util.Helper;
+
+public class MetaMeeting extends MetaGroup {
+    public MetaMeeting() {
+        super();
+    }
+
+    public MetaMeeting(ReferenceWrapper rw) {
+        super(rw);
+    }
+    public void pushMeeting(Meeting meeting, Group group) {
+        String mid = meeting.getId().getId();
+        Pair binding = new Pair(mid, group.getGroupID().getId());
+        db.select("meetings").select(mid).setVal(meeting);
+        db.select("meetGroups").select(Helper.hashCode(binding)).setVal(binding);
+    }
+
+}
---
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/MetaMeetingTest.java
@@ -0,0 +1,44 @@
+package ch.epfl.sweng.studdybuddy;
+
+import com.google.firebase.database.DatabaseReference;
+
+import org.junit.Test;
+
+import ch.epfl.sweng.studdybuddy.core.Group;
+import ch.epfl.sweng.studdybuddy.core.Meeting;
+import ch.epfl.sweng.studdybuddy.core.Pair;
+import ch.epfl.sweng.studdybuddy.firebase.FirebaseReference;
+import ch.epfl.sweng.studdybuddy.firebase.MetaMeeting;
+import ch.epfl.sweng.studdybuddy.firebase.ReferenceWrapper;
+import ch.epfl.sweng.studdybuddy.util.Helper;
+
+import static ch.epfl.sweng.studdybuddy.MetaFactory.deepFBReference;
+import static ch.epfl.sweng.studdybuddy.util.CoreFactory.blankGroupWId;
+import static ch.epfl.sweng.studdybuddy.util.CoreFactory.randomMeeting;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MetaMeetingTest {
+    private DatabaseReference testref = deepFBReference();
+   @Test
+   public void pushMeetingArguments() {
+       ReferenceWrapper wb = mock(FirebaseReference.class);
+       when(wb.select(any())).thenReturn(wb);
+       MetaMeeting mm = new MetaMeeting(wb);
+       Meeting m = randomMeeting();
+       String mid = m.getId().getId();
+       Group bg = blankGroupWId("-");
+       mm.pushMeeting(m, bg);
+       Pair binding = new Pair(mid, bg.getGroupID().getId());
+       when(testref.child(any())).thenReturn(testref);
+       verify(wb, times(1)).select("meetings");
+       verify(wb, times(1)).select("meetGroups");
+       verify(wb, times(1)).select(mid);
+       verify(wb, times(1)).select(Helper.hashCode(binding));
+       verify(wb, times(1)).setVal(m);
+       verify(wb, times(1)).setVal(any(Pair.class));
+   }
+}
