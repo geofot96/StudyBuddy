393703bcdf9f1884da99564f26fbb463741c7f1a
underwoodkats <underwoodkats@gmail.com>
2018-11-15 11:16:39 +0100

    added new test for getCreationMethod in case that months is lower that 10 and day also is lower that 10

--- app/src/test/java/ch/epfl/sweng/studdybuddy/GroupsRecyclerAdapterTest.java
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/GroupsRecyclerAdapterTest.java
@@ -2,10 +2,13 @@ package ch.epfl.sweng.studdybuddy;
 
 import org.junit.Test;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import ch.epfl.sweng.studdybuddy.core.Course;
 import ch.epfl.sweng.studdybuddy.core.Group;
+import ch.epfl.sweng.studdybuddy.core.SerialDate;
 import ch.epfl.sweng.studdybuddy.util.GroupsRecyclerAdapter;
 
 import static junit.framework.TestCase.assertEquals;
@@ -18,15 +21,29 @@ public class GroupsRecyclerAdapterTest {
         Course testCourse = new Course("TestCourse");
         Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
         String date = GroupsRecyclerAdapter.getCreationDate(testGroup);
-
         Date targetDate = new Date();
         int day = targetDate.getDate();
         int month = targetDate.getMonth();
         int year = targetDate.getYear();
-
         String target = String.valueOf(day) + "-" + String.valueOf(month + 1) + "-" + String.valueOf(year + 1900);
-
-
         assertEquals(target, date);
+    }
+
+    @Test
+    public void getCreationDateMethodGetRightFormatOfDateBeginningOfMonth() {
+
+        Course testCourse = new Course("TestCourse");
+        Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
+
+        SerialDate serialDate = new SerialDate();
+        serialDate.setSeconds(2);
+        serialDate.setMinutes(1);
+        serialDate.setDate(2);
+        serialDate.setMonth(2);
+        serialDate.setYear(118);
+
+        testGroup.setCreationDate(serialDate);
+        String result = GroupsRecyclerAdapter.getCreationDate(testGroup);
+        assertEquals("02-03-2018", result);
     }
 }
