c197d08f03984b32ed892d21f5c648854cea8cd9
underwoodkats <underwoodkats@gmail.com>
2018-11-16 18:33:01 +0100

    added 3 tests for getCreationDayMethod in order to test each possible path

--- app/src/test/java/ch/epfl/sweng/studdybuddy/GroupsRecyclerAdapterTest.java
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/GroupsRecyclerAdapterTest.java
@@ -16,21 +16,7 @@ import static junit.framework.TestCase.assertEquals;
 public class GroupsRecyclerAdapterTest {
 
     @Test
-    public void getCreationDateMethodGetRightFormatOfDateFromGroup(){
-
-        Course testCourse = new Course("TestCourse");
-        Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
-        String date = GroupsRecyclerAdapter.getCreationDate(testGroup);
-        Date targetDate = new Date();
-        int day = targetDate.getDate();
-        int month = targetDate.getMonth();
-        int year = targetDate.getYear();
-        String target = String.valueOf(day) + "-" + String.valueOf(month + 1) + "-" + String.valueOf(year + 1900);
-        assertEquals(target, date);
-    }
-
-    @Test
-    public void getCreationDateMethodGetRightFormatOfDateBeginningOfMonth() {
+    public void getCreationDateMethodMonthAndDayLessThenTen() {
 
         Course testCourse = new Course("TestCourse");
         Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
@@ -48,7 +34,25 @@ public class GroupsRecyclerAdapterTest {
     }
 
     @Test
-    public void getCreationDateMethodGetRightFormatOfDateBeginningOfMonthAndMothInTheEndOfTheYear() {
+    public void getCreationDateMethodMonthAndDayMoreThenTen() {
+
+        Course testCourse = new Course("TestCourse");
+        Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
+
+        SerialDate serialDate = new SerialDate();
+        serialDate.setSeconds(2);
+        serialDate.setMinutes(1);
+        serialDate.setDate(15);
+        serialDate.setMonth(11);
+        serialDate.setYear(118);
+
+        testGroup.setCreationDate(serialDate);
+        String result = GroupsRecyclerAdapter.getCreationDate(testGroup);
+        assertEquals("15-12-2018", result);
+    }
+
+    @Test
+    public void getCreationDateMethodMonthMoreThenTenAndDayLessThenTen() {
 
         Course testCourse = new Course("TestCourse");
         Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
@@ -57,11 +61,29 @@ public class GroupsRecyclerAdapterTest {
         serialDate.setSeconds(2);
         serialDate.setMinutes(1);
         serialDate.setDate(2);
-        serialDate.setMonth(10);
+        serialDate.setMonth(11);
         serialDate.setYear(118);
 
         testGroup.setCreationDate(serialDate);
         String result = GroupsRecyclerAdapter.getCreationDate(testGroup);
-        assertEquals("02-11-2018", result);
+        assertEquals("02-12-2018", result);
+    }
+
+    @Test
+    public void getCreationDateMethodDayMoreThenTenAndMonthLessThenTen() {
+
+        Course testCourse = new Course("TestCourse");
+        Group testGroup = new Group(6, testCourse, "rus", "123", "1231");
+
+        SerialDate serialDate = new SerialDate();
+        serialDate.setSeconds(2);
+        serialDate.setMinutes(1);
+        serialDate.setDate(15);
+        serialDate.setMonth(2);
+        serialDate.setYear(118);
+
+        testGroup.setCreationDate(serialDate);
+        String result = GroupsRecyclerAdapter.getCreationDate(testGroup);
+        assertEquals("15-03-2018", result);
     }
 }
