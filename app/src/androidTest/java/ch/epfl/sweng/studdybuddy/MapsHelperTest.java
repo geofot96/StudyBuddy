897ed24df0daa674da1eda841f1a3beb0aae192b
Boubacar Camara <bakarsadio@gmail.com>
2018-11-20 11:08:01 +0100

    Try again with permission rule

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsHelperTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsHelperTest.java
@@ -3,10 +3,12 @@ package ch.epfl.sweng.studdybuddy;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.studdybuddy.activities.MapsActivity;
 
@@ -14,19 +16,17 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.core.IsNot.not;
-
+@RunWith(AndroidJUnit4.class)
 public class MapsHelperTest {
 
     @Rule
     public ActivityTestRule<MapsActivity> mIntentsTestRule = new ActivityTestRule<>(MapsActivity.class);
 
-
-    GrantPermissionRule permissionRule;
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
     @Before
-    public void setup(){
-        permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
-    }
+    public void setup(){}
     @Test
     public void acceptMeetingsTest(){
         try {
