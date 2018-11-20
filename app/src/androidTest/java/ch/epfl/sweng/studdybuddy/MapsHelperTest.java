2a98fe1101068dc794dece265c466af8262f0f51
Boubacar Camara <bakarsadio@gmail.com>
2018-11-20 03:46:26 +0100

    Try with permissionRule

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsHelperTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsHelperTest.java
@@ -2,7 +2,9 @@ package ch.epfl.sweng.studdybuddy;
 
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -18,10 +20,22 @@ public class MapsHelperTest {
     @Rule
     public ActivityTestRule<MapsActivity> mIntentsTestRule = new ActivityTestRule<>(MapsActivity.class);
 
+
+    GrantPermissionRule permissionRule;
+
+    @Before
+    public void setup(){
+        permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+    }
     @Test
     public void acceptMeetingsTest(){
+        try {
+            Thread.sleep(2000);
+            onView(withId(R.id.confirmLocation)).check(matches(not(ViewMatchers.isDisplayed())));
 
-        onView(withId(R.id.confirmLocation)).check(matches(not(ViewMatchers.isDisplayed())));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 
     }
