898b614351b9ffe730dc0d60ed9da7e490744752
Boubacar Camara <bakarsadio@gmail.com>
2018-11-20 02:48:42 +0100

    Put back  Mapstest

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsHelperTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsHelperTest.java
@@ -1,22 +1,27 @@
 package ch.epfl.sweng.studdybuddy;
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.ViewMatchers;
+
+import org.junit.Rule;
 import org.junit.Test;
+
+import ch.epfl.sweng.studdybuddy.activities.MapsActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static java.util.regex.Pattern.matches;
+import static org.hamcrest.core.IsNot.not;
 
 public class MapsHelperTest {
 
-   /* @Rule
+    @Rule
     public IntentsTestRule<MapsActivity> mIntentsTestRule = new IntentsTestRule<>(MapsActivity.class);
-*/
+
     @Test
     public void acceptMeetingsTest(){
-       /* List<Meeting> meetings, meetingsFb;
-        meetings = new ArrayList<>();
-        Meeting m = new Meeting("a");
-        m.setLocation(new MeetingLocation("a", "b", 1,2));
-        meetingsFb = new ArrayList<Meeting>();
-        meetingsFb.add(m);
+
         onView(withId(R.id.confirmLocation)).check(matches(not(ViewMatchers.isDisplayed())));
 
-*/
     }
 }
