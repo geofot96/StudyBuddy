8587074c2b4f81c1a41f3b854977da4be789ba74
Yann BOUQUET <y.bouquet29@gmail.com>
2018-11-27 10:45:05 +0100

    uncomment mapsActivity

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/MapsActivityTest.java
@@ -33,9 +33,6 @@ import static org.mockito.Mockito.mock;
 
 @RunWith(AndroidJUnit4.class)
 public class MapsActivityTest {
-  @Test
-  public void EmptyTest(){}
-/*
     @Rule
     public ActivityTestRule<MapsActivity> mIntentsTestRule = new ActivityTestRule<>(MapsActivity.class);
 
