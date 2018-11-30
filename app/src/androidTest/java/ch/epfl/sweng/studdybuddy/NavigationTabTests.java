3b08710b735004e4de86c8cdf8242f04ae782918
Boubacar Camara <bakarsadio@gmail.com>
2018-11-30 22:43:38 +0100

    Modularize navigation UI in tests

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/NavigationTabTests.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/NavigationTabTests.java
@@ -1,6 +1,5 @@
 package ch.epfl.sweng.studdybuddy;
 
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,14 +9,6 @@ import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.studdybuddy.activities.NavigationActivity;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static ch.epfl.sweng.studdybuddy.GroupsActivityLeadsToCreateGroup.childAtPosition;
-import static org.hamcrest.Matchers.allOf;
-
 @RunWith(AndroidJUnit4.class)
 public class NavigationTabTests
 {
@@ -25,45 +16,23 @@ public class NavigationTabTests
     @Rule
     public ActivityTestRule<NavigationActivity> mActivityTestRule = new ActivityTestRule<>(NavigationActivity.class);
 
-    @Test
-    public void testClickOnProfile()
-    {
-        ViewInteraction bottomNavigationItemView = onView(
-                allOf(withId(R.id.navToProfile), withContentDescription("Profile"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.bottom_navigation),
-                                        0),
-                                1),
-                        isDisplayed()));
-        bottomNavigationItemView.perform(click());
-    }
 
     @Test
     public void testClickOnHome()
     {
-        ViewInteraction bottomNavigationItemView4 = onView(
-                allOf(withId(R.id.navToHome), withContentDescription("Home"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.bottom_navigation),
-                                        0),
-                                0),
-                        isDisplayed()));
-        bottomNavigationItemView4.perform(click());
+
+        NavigationTestHelper.navigate("Home", R.id.navToHome, 0);
+    }
+
+    @Test
+    public void testClickOnProfile()
+    {
+        NavigationTestHelper.navigate("Profile", R.id.navToProfile, 1);
     }
 
     @Test
     public void testClickOnChat()
     {
-        ViewInteraction bottomNavigationItemView4 = onView(
-                allOf(withId(R.id.navToChat), withContentDescription("Chat"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.bottom_navigation),
-                                        0),
-                                2),
-                        isDisplayed()));
-        bottomNavigationItemView4.perform(click());
+        NavigationTestHelper.navigate("Chat", R.id.navToChat, 2);
     }
 }
--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/SettingsFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/SettingsFragmentTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.sweng.studdybuddy;
 
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -18,11 +17,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static ch.epfl.sweng.studdybuddy.GroupsActivityLeadsToCreateGroup.childAtPosition;
-import static org.hamcrest.Matchers.allOf;
+import static ch.epfl.sweng.studdybuddy.NavigationTestHelper.navigate;
 
 @RunWith(AndroidJUnit4.class)
 public class SettingsFragmentTest {
@@ -34,15 +30,7 @@ public class SettingsFragmentTest {
 
     @Before
     public void goToSettings(){
-        ViewInteraction settings = onView(
-                allOf(withId(R.id.navToSettings), withContentDescription("Settings"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.bottom_navigation),
-                                        0),
-                                3),
-                        isDisplayed()));
-        settings.perform(click());
+        navigate("Settings", R.id.navToSettings,3);
     }
 
     @Test
