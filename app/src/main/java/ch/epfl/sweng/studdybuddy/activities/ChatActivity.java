9d3880faf074e1aba8d1bf5018fd9b40acb2bcc1
Gerald Sula <gerald.sula@epfl.ch>
2018-12-01 17:24:01 +0100

    Remove some unnecessary old methods

--- app/src/main/java/ch/epfl/sweng/studdybuddy/activities/ChatActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/activities/ChatActivity.java
@@ -299,21 +299,6 @@ public class ChatActivity extends AppCompatActivity
                 FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), downloadUri));
     }
 
-    public Uri getImageUri(Context inContext, Bitmap inImage)
-    {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri)
-    {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
 
 
     public FirebaseReference initRef()
