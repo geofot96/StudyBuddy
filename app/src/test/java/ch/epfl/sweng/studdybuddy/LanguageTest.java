fddb7d670d0ecd5cfe594ac0c313a3cbb0440674
underwoodkats <underwoodkats@gmail.com>
2018-12-03 22:20:49 +0100

    added tests for Language class

--- app/src/test/java/ch/epfl/sweng/studdybuddy/LanguageTest.java
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/LanguageTest.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 
 import ch.epfl.sweng.studdybuddy.util.Language;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class LanguageTest {
+
     @Test
     public void CorrectLangTest(){
         assertTrue(Language.EN.equals("\uD83C\uDDEC\uD83C\uDDE7"));
@@ -14,5 +16,26 @@ public class LanguageTest {
         assertTrue(Language.GER.equals("\uD83C\uDDE9\uD83C\uDDEA"));
         assertTrue(Language.IT.equals("\uD83C\uDDEE\uD83C\uDDF9"));
         assertTrue(Language.languages.size() == 4);
+    }
+
+    @Test
+    public void EnglishToZero(){
+        assertEquals(0, Language.LanguageToInt("\uD83C\uDDEC\uD83C\uDDE7"));
+    }
+    @Test
+    public void FrenchToOne(){
+        assertEquals(1, Language.LanguageToInt("\uD83C\uDDEB\uD83C\uDDF7"));
+    }
+    @Test
+    public void GermanToTwo(){
+        assertEquals(2, Language.LanguageToInt("\uD83C\uDDE9\uD83C\uDDEA"));
+    }
+    @Test
+    public void ItalianToThree(){
+        assertEquals(3, Language.LanguageToInt("\uD83C\uDDEE\uD83C\uDDF9"));
+    }
+    @Test
+    public void DefaultToZero(){
+        assertEquals(0, Language.LanguageToInt("asdasd"));
     }
 }
