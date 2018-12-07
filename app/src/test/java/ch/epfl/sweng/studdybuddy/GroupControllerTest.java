db0d546fb1b15474cc32d13fa745f42a414beaf4
Frederic Gessler <frederic.gessler@epfl.ch>
2018-12-07 20:34:29 +0100

    Test processResult::GroupController

--- app/src/test/java/ch/epfl/sweng/studdybuddy/GroupControllerTest.java
+++ app/src/test/java/ch/epfl/sweng/studdybuddy/GroupControllerTest.java
@@ -5,14 +5,19 @@ import org.junit.Test;
 import ch.epfl.sweng.studdybuddy.core.Group;
 import ch.epfl.sweng.studdybuddy.firebase.MetaGroupAdmin;
 import ch.epfl.sweng.studdybuddy.tools.Intentable;
+import ch.epfl.sweng.studdybuddy.tools.Resultable;
 
 import static ch.epfl.sweng.studdybuddy.controllers.GroupController.leaveOnClick;
+import static ch.epfl.sweng.studdybuddy.controllers.GroupController.processResult;
 import static ch.epfl.sweng.studdybuddy.util.CoreFactory.blankGroupWId;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static android.app.Activity.RESULT_OK;
 
 public class GroupControllerTest {
+
+    Resultable res = mock(Resultable.class);
     @Test
     public void testLeaveOnClick() {
         MetaGroupAdmin mga = mock(MetaGroupAdmin.class);
@@ -23,5 +28,23 @@ public class GroupControllerTest {
         verify(mga, times(1)).clearListeners();
         verify(mga, times(1)).removeUserFromGroup(uId, group);
         verify(destination, times(1)).launch();
+    }
+
+    @Test
+    public void testProcessResultInvalidResultCode() {
+        processResult(1, -1111, res);
+        verify(res, times(0)).onResult();
+    }
+
+    @Test
+    public void testProcessResultInvalidRequestCode() {
+        processResult(-1, RESULT_OK, res);
+        verify(res, times(0)).onResult();
+    }
+
+    @Test
+    public void testProcessResultValid() {
+        processResult(1, RESULT_OK, res);
+        verify(res, times(1)).onResult();
     }
 }
