5b9274d0ff3bb7984c0cb36987c02b0de2bcc8d1
Boubacar Camara <bakarsadio@gmail.com>
2018-12-07 18:26:06 +0100

    Add Thread.sleep in signoutButton Test

--- app/src/androidTest/java/ch/epfl/sweng/studdybuddy/SettingsFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studdybuddy/SettingsFragmentTest.java
@@ -101,8 +101,9 @@ public class SettingsFragmentTest {
     }
 
     @Test
-    public void signoutButtonTest(){
+    public void signoutButtonTest() throws InterruptedException {
         onView(withId(R.id.btn_sign_out)).perform(click());
+        Thread.sleep(1000);
         onView(withId(R.id.googleBtn)).check(matches(isDisplayed()));
     }
 
