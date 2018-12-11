48dc36d23cff139a84be26e5471d7b87322b5306
underwoodkats <underwoodkats@gmail.com>
2018-12-12 00:23:40 +0100

    added setUP fiunctions to AddFriendsActivity

--- app/src/main/java/ch/epfl/sweng/studdybuddy/activities/AddFriendsActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/activities/AddFriendsActivity.java
@@ -1,8 +1,10 @@
 package ch.epfl.sweng.studdybuddy.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -11,120 +13,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.sweng.studdybuddy.R;
+import ch.epfl.sweng.studdybuddy.core.User;
+import ch.epfl.sweng.studdybuddy.firebase.FirebaseReference;
+import ch.epfl.sweng.studdybuddy.firebase.MetaGroupAdmin;
 import ch.epfl.sweng.studdybuddy.firebase.ReferenceWrapper;
+import ch.epfl.sweng.studdybuddy.tools.Intentable;
+import ch.epfl.sweng.studdybuddy.util.StudyBuddy;
+
+import static ch.epfl.sweng.studdybuddy.controllers.CourseSelectController.onClickAddCourse;
+import static ch.epfl.sweng.studdybuddy.controllers.CourseSelectController.resetSelectViews;
+import static ch.epfl.sweng.studdybuddy.controllers.CourseSelectController.updateCoursesOnDone;
+import static ch.epfl.sweng.studdybuddy.util.ActivityHelper.onClickLaunch;
+import static ch.epfl.sweng.studdybuddy.util.ActivityHelper.showDropdown;
 
 public class AddFriendsActivity extends AppCompatActivity {
     static ReferenceWrapper firebase;
     static List<String> friendsDB;
-
-    //List of selected friends
-    public static final List<String> friendsSelection = new ArrayList<>();
-
-
+    static AutoCompleteTextView autocompleteFriends;
+    public static final List<String> friendSelection = new ArrayList<>();
+    public static ArrayAdapter<String> adapter;
     static AutoCompleteTextView autocompleteFriends;
     public static ArrayAdapter<String> adapterFriends;
+    String uId;
     static Button addButton;
+    private MetaGroupAdmin mga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        firebase  = new FirebaseReference();
+        mga = new MetaGroupAdmin(firebase);
         setContentView(R.layout.activity_find_friends);
-
-        Intent other = getIntent();
-        friendsDB = new ArrayList<>();
-        friendsDB.add("Untitled");
-
-//        setUpDb(setUpAutoComplete());
-//        setUpButtons();
-//        setUpSelectedCourses();
-
+        setUpButtons();
+        setUpSelectedCourses();
+        setUpDb(setUpAutoComplete());
     }
 
-//    private void setUpButtons() {
-//        final Intent toMain = new Intent(this, NavigationActivity.class);
-//        Button skipButton = findViewById(R.id.skipButton);
-//        skipButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v)  {
-//                startActivity(toMain);
-//            }
-//        });
-//        doneButton = findViewById(R.id.doneButton);
-//        doneButton.setEnabled(false);
-//        doneButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                User currentUser = ((StudyBuddy) CourseSelectActivity.this.getApplication()).getAuthendifiedUser();
-//                new MetaGroupAdmin().putAllCourses(courseSelection, currentUser.getUserID().getId());
-//                startActivity(toMain);
-//            }
-//        });
-//    }
+    private void setUpButtons() {
+        Intentable i = new Intentable(this, new Intent(this, NavigationActivity.class));
+        addButton = findViewById(R.id.btn_add);
+        User currentUser = ((StudyBuddy) AddFriendsActivity.this.getApplication()).getAuthendifiedUser();
+        uId = currentUser.getUserID().getId();
+        addButton.setOnClickListener(updateCoursesOnDone(currentUser, friendSelection, mga, i));
+    }
 
-//    private ArrayAdapter<String> setUpAutoComplete(){
-//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, coursesDB);
-//        autocomplete = (AutoCompleteTextView) findViewById(R.id.courseComplete);
-//        autocomplete.setAdapter(adapter);
-//        autocomplete.setOnClickListener(showDropdown(autocomplete));
-//        autocomplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String textInput = parent.getAdapter().getItem(position).toString();
-//                if(!courseSelection.contains(textInput)) { addCourse(textInput); }
-//            }
-//        });
-//        return adapter;
-//    }
+    private ArrayAdapter<String> setUpAutoComplete(){
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, friendsDB);
+        autocompleteFriends = (AutoCompleteTextView) findViewById(R.id.friendsComplete);
+        autocompleteFriends.setAdapter(adapter);
+        autocompleteFriends.setOnClickListener(showDropdown(autocompleteFriends));
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        autocompleteFriends.setOnItemClickListener(onClickAddCourse(friendSelection, resetSelectViews(addButton, autocompleteFriends, imm)));
+        return adapter;
+    }
 
-//    private void setUpSelectedCourses() {
-//
-//        final RecyclerView selectedCourses = (RecyclerView) findViewById(R.id.coursesSet);
-//        selectedCourses.setLayoutManager(new LinearLayoutManager(this));
-//
-//        selectedCourses.setAdapter(new CourseAdapter(courseSelection));
-//       /*ItemTouchHelper mIth = new ItemTouchHelper(
-//               new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,
-//                       ItemTouchHelper.RIGHT)
-//               {
-//                   @Override
-//                   public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1)
-//                   {
-//                       return false;
-//                   }
-//
-//                   @Override
-//                   public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i)
-//                   {
-//                       Holder cc = (Holder) viewHolder;
-//                       courseSelection.remove(courseSelection.indexOf(cc.get()));
-//                       selectedCourses.getAdapter().notifyDataSetChanged();
-//                       if(courseSelection.size() == 0)
-//                           doneButton.setEnabled(false);
-//                   }
-//               });
-//       mIth.attachToRecyclerView(selectedCourses);*/
-//    }
-//
-//    private void setUpDb(ArrayAdapter<String> adapter) {
-//        firebase = new FirebaseReference(FirebaseDatabase.getInstance().getReference());
-//        firebase.select("courses").getAll(String.class, AdapterConsumer.adapterConsumer(String.class, coursesDB, new ArrayAdapterAdapter(adapter)));
-//    }
-//
-//    private void addCourse(String course)
-//    {
-//        courseSelection.add(course);
-//        //Dismiss KB
-//        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-//        imm.hideSoftInputFromWindow(autocomplete.getWindowToken(), 0);
-//        //reset search text
-//        autocomplete.setText("");
-//        doneButton.setEnabled(true);
-//    }
-//
-//    private void removeCourse(String course)
-//    {
-//        courseSelection.remove(course);
-//    }
+
 
 }
