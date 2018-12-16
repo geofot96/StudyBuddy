30300e2c03fda54ba2c93407c2caececf1c45af8
Gerald Sula <gerald.sula@epfl.ch>
2018-12-16 19:00:42 +0100

    Comment all Course class

--- app/src/main/java/ch/epfl/sweng/studdybuddy/core/Course.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/core/Course.java
@@ -7,10 +7,17 @@ package ch.epfl.sweng.studdybuddy.core;
 public class Course
 {
     private String courseName;
-    private ID<Course> courseID; //mustn't be final because it will be reset when we read from the DB #kiru
+    private ID<Course> courseID;
 
+    /**
+     * Empty constructor
+     */
     public Course() {}
 
+    /**
+     * Create a course from a Course name
+     * @param courseName the course name
+     */
     public Course(String courseName)
     {
         this();
@@ -18,24 +25,44 @@ public class Course
         courseID = new ID<>(courseName);
     }
 
+    /**
+     * Create a new course from an existing one
+     * @param sourceCourse the course to be copied
+     */
     public Course(Course sourceCourse)
     {
         this.courseName = sourceCourse.getCourseName();
         this.courseID = sourceCourse.getCourseID();
     }
 
+    /**
+     *
+     * @return the name of the course
+     */
     public String getCourseName()
     {
         return  courseName;
     }
 
+    /**
+     * Set a new name to the course
+     * @param name the name to be set
+     */
     public void setCourseName(String name) { courseName = name; }
 
+    /**
+     *
+     * @return the id of the course
+     */
     public ID<Course> getCourseID()
     {
         return courseID;
     }
 
+    /**
+     * Set a new id to the course
+     * @param courseID the id to be set
+     */
     public void setCourseID(ID<Course> courseID)
     {
         this.courseID = courseID;
