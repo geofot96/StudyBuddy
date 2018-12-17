5a0acaabe3e552557e7e66a082d9a1085ba4a67d
Gerald Sula <gerald.sula@epfl.ch>
2018-12-17 18:19:22 +0100

    Clean up CourseSelectActivity and add header

--- app/src/main/java/ch/epfl/sweng/studdybuddy/activities/CourseSelectActivity.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/activities/CourseSelectActivity.java
@@ -1,23 +1,16 @@
 package ch.epfl.sweng.studdybuddy.activities;
 
-import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,15 +20,12 @@ import ch.epfl.sweng.studdybuddy.core.User;
 import ch.epfl.sweng.studdybuddy.firebase.FirebaseReference;
 import ch.epfl.sweng.studdybuddy.firebase.MetaGroupAdmin;
 import ch.epfl.sweng.studdybuddy.firebase.ReferenceWrapper;
-import ch.epfl.sweng.studdybuddy.tools.Adapter;
 import ch.epfl.sweng.studdybuddy.tools.AdapterAdapter;
 import ch.epfl.sweng.studdybuddy.tools.AdapterConsumer;
 import ch.epfl.sweng.studdybuddy.tools.ArrayAdapterAdapter;
 import ch.epfl.sweng.studdybuddy.tools.CourseAdapter;
-import ch.epfl.sweng.studdybuddy.tools.Holder;
 import ch.epfl.sweng.studdybuddy.tools.Intentable;
 import ch.epfl.sweng.studdybuddy.tools.RecyclerAdapterAdapter;
-import ch.epfl.sweng.studdybuddy.util.Helper;
 import ch.epfl.sweng.studdybuddy.util.StudyBuddy;
 
 import static ch.epfl.sweng.studdybuddy.controllers.CourseSelectController.deleteCourseOnSwipe;
@@ -46,7 +36,9 @@ import static ch.epfl.sweng.studdybuddy.controllers.CourseSelectController.updat
 import static ch.epfl.sweng.studdybuddy.util.ActivityHelper.onClickLaunch;
 import static ch.epfl.sweng.studdybuddy.util.ActivityHelper.showDropdown;
 
-
+/**
+ * Activity offering to the user the possibility to add a preferred course to his account
+ */
 public class CourseSelectActivity extends AppCompatActivity {
     static List<String> coursesDB = new ArrayList<>();
     private final List<String> courseSelection = new ArrayList<>();
@@ -60,7 +52,7 @@ public class CourseSelectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        firebase  = new FirebaseReference();
+        firebase = new FirebaseReference();
         mga = new MetaGroupAdmin(firebase);
         setContentView(R.layout.activity_course_select);
         setUpButtons();
@@ -78,7 +70,7 @@ public class CourseSelectActivity extends AppCompatActivity {
         doneButton.setOnClickListener(updateCoursesOnDone(currentUser, courseSelection, mga, i));
     }
 
-    private ArrayAdapter<String> setUpAutoComplete(){
+    private ArrayAdapter<String> setUpAutoComplete() {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, coursesDB);
         autocomplete = (AutoCompleteTextView) findViewById(R.id.courseComplete);
         autocomplete.setAdapter(adapter);
@@ -88,20 +80,20 @@ public class CourseSelectActivity extends AppCompatActivity {
         return adapter;
     }
 
-   private void setUpSelectedCourses() {
-       final RecyclerView selectedCourses = (RecyclerView) findViewById(R.id.coursesSet);
-       RecyclerView.Adapter courseAdapter = new CourseAdapter(courseSelection);
-       selectedCourses.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-       selectedCourses.setAdapter(courseAdapter);
-       AdapterAdapter adapterC = new RecyclerAdapterAdapter(courseAdapter);
-       mga.addListenner(adapterC);
-       mga.addListenner(updateClickable(doneButton, courseSelection));
-       mga.getUserCourses(uId, courseSelection);
-       ItemTouchHelper swipeCourse = deleteCourseOnSwipe(courseSelection, doneButton, adapterC);
-       swipeCourse.attachToRecyclerView(selectedCourses);
-   }
+    private void setUpSelectedCourses() {
+        final RecyclerView selectedCourses = (RecyclerView) findViewById(R.id.coursesSet);
+        RecyclerView.Adapter courseAdapter = new CourseAdapter(courseSelection);
+        selectedCourses.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+        selectedCourses.setAdapter(courseAdapter);
+        AdapterAdapter adapterC = new RecyclerAdapterAdapter(courseAdapter);
+        mga.addListenner(adapterC);
+        mga.addListenner(updateClickable(doneButton, courseSelection));
+        mga.getUserCourses(uId, courseSelection);
+        ItemTouchHelper swipeCourse = deleteCourseOnSwipe(courseSelection, doneButton, adapterC);
+        swipeCourse.attachToRecyclerView(selectedCourses);
+    }
 
-   private void setUpDb(ArrayAdapter<String> adapter) {
-       firebase.select("courses").getAll(String.class, AdapterConsumer.adapterConsumer(String.class, coursesDB, new ArrayAdapterAdapter(adapter)));
-   }
+    private void setUpDb(ArrayAdapter<String> adapter) {
+        firebase.select("courses").getAll(String.class, AdapterConsumer.adapterConsumer(String.class, coursesDB, new ArrayAdapterAdapter(adapter)));
+    }
 }
