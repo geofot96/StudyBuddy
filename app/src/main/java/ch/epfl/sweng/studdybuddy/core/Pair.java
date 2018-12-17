f51dd43f9bc20107121e5e5506541b25c7f1392b
Gerald Sula <gerald.sula@epfl.ch>
2018-12-17 18:54:49 +0100

    Comment Pair class

--- app/src/main/java/ch/epfl/sweng/studdybuddy/core/Pair.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/core/Pair.java
@@ -7,27 +7,49 @@ public class Pair {
     String key;
     String value;
 
+    /**
+     * Empty constructor
+     */
     public Pair(){}
 
+    /**
+     * Pair to be created with strings
+     * @param key the key string
+     * @param value the value sting
+     */
     public Pair(String key, String value){
         this();
         this.key = key;
         this.value = value;
     }
 
+    /**
+     * Getter of the key string
+     * @return the key of the pair
+     */
     public String getKey() {
         return key;
     }
-
+    /**
+     * Getter of the value string
+     * @return the value of the pair
+     */
     public String getValue() {
         return value;
     }
 
-
+    /**
+     * Set a new key string
+     * @param key the new key string
+     */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /**
+     * Set a new value string
+     * @param value the new value string
+     */
     public void setValue(String value) {
         this.value = value;
     }
