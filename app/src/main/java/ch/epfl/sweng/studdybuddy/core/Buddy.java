a7d32e9e38800f6dd1703dd8815ef18cb5bc5c76
Boubacar Camara <bakarsadio@gmail.com>
2018-12-18 22:14:12 +0100

    Add tests for buddy

--- app/src/main/java/ch/epfl/sweng/studdybuddy/core/Buddy.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/core/Buddy.java
@@ -1,40 +1,21 @@
 package ch.epfl.sweng.studdybuddy.core;
 
-import java.util.Date;
-
+/**
+ * Class representing a two friends
+ */
 public final class Buddy {
 
-
-    public String getKey() {
-        return buddies.getKey();
-    }
-
-    public void setKey(String alice) {
-        buddies.setKey(alice);
-    }
-
-    public String getValue() {
-        return buddies.getValue();
-    }
-
-    public void setValue(String bob) {
-        buddies.setValue(bob);
-    }
-
-    public Long getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Long creationDate) {
-        this.creationDate = creationDate;
-    }
-
     private Pair buddies;
-    private Long creationDate;
+
     public Buddy(){}
-    public Buddy(String alice, String bob) {
-        creationDate = new Date().getTime();
-        buddies = new Pair(alice, bob);
+
+    /**
+     * Construct buddies
+     * @param aliceID the name of the first person id as a String
+     * @param bobID the name of the second person id as a String
+     */
+    public Buddy(String aliceID, String bobID) {
+        buddies = new Pair(aliceID, bobID);
     }
 
     //Returns null if not buddy, because no Optionnal for Java<8
@@ -48,6 +29,41 @@ public final class Buddy {
         return null;
     }
 
+    /*
+     * Returns the first person id as a String
+     */
+    public String getKey() {
+        return buddies.getKey();
+    }
+
+    /**
+     * Sets the first person id to the one given as parameter
+     * @param aliceID the new id
+     */
+    public void setKey(String aliceID) {
+        buddies.setKey(aliceID);
+    }
+
+    /*
+     * Returns the second person id as a String
+     */
+    public String getValue() {
+        return buddies.getValue();
+    }
+
+    /**
+     * Sets the second person id to the one given as parameter
+     * @param bobID the new id
+     */
+    public void setValue(String bobID) {
+        buddies.setValue(bobID);
+    }
+
+    /**
+     * Returns the hash of the pair of buddies.
+     * Note that this hash is symetric. An inverted pair of buddies will lead to the same hash
+     * @return
+     */
     public String hash() {
         return Integer.toHexString(getKey().hashCode() ^ getValue().hashCode());
     }
