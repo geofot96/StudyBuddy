6b72502cc4595a95971384ef96277dca0481522b
Gerald Sula <gerald.sula@epfl.ch>
2018-12-18 20:19:29 +0100

    Comment CourseAdapter header

--- app/src/main/java/ch/epfl/sweng/studdybuddy/tools/CourseAdapter.java
+++ app/src/main/java/ch/epfl/sweng/studdybuddy/tools/CourseAdapter.java
@@ -4,27 +4,29 @@ import java.util.List;
 
 import ch.epfl.sweng.studdybuddy.R;
 
-public class CourseAdapter extends Adapter
-{    List<String> list;
+/**
+ * An adapter specialized in Courses
+ */
+public class CourseAdapter extends Adapter {
+
+    List<String> list;
 
     //ajouter un constructeur prenant en entrée une liste
-    public CourseAdapter(List<String> list)
-    {
-        super( R.layout.course_card, R.id.text);
+
+    public CourseAdapter(List<String> list) {
+        super(R.layout.course_card, R.id.text);
         this.list = list;
     }
 
     //c'est ici que nous allons remplir notre cellule avec le texte/image de chaque MyObjects
     @Override
-    public void onBindViewHolder(Holder holder, int position)
-    {
+    public void onBindViewHolder(Holder holder, int position) {
         String course = list.get(position);
         holder.bind(course);
     }
 
     @Override
-    public int getItemCount()
-    {
+    public int getItemCount() {
         return list.size();
     }
 
